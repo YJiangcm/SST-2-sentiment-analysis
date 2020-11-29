552b4d1fe79da127d1abb98311c0cf4b4e9215e1
YJiangcm <70904956+YJiangcm@users.noreply.github.com>
2020-11-30 05:01:09 +0800

    Update data_sst2.py

--- data_sst2.py
+++ data_sst2.py
@@ -26,48 +26,42 @@ class DataPrecessForSentence(Dataset):
     # Get text and label
     def get_input(self, df):
         """
-        Through the process of word segmentation, IDization, truncation, and filling of the input text, 
-        the final sequence that can be used for model input is obtained.
-        
-        Input parameters:
-            dataset          : pandas dataframe.
-        Output parameters:
-            seq              : The'CLS' and'SEP' symbols are respectively spliced at the head and tail of the input parameter seq.
-                               If the length is still less than max_seq_len, 0 is used to fill the tail.
-            seq_mask         : Only the sequence containing 0, 1 and the length equal to seq is used to characterize whether the symbol in seq is                                  meaningful.
-            seq_segment      : Because it is a single sentence, the value is all 0.
-            labels           : {0,1}.
         """
-        sentences_1 = df['s1'].values
+        sentences = df['s1'].values
         labels = df['similarity'].values
         # tokenizer
-        tokens_seq_1 = list(map(self.bert_tokenizer.tokenize, sentences_1)) # list of shape [sentence_len, token_len]
+        tokens_seq = list(map(self.bert_tokenizer.tokenize, sentences)) # list of shape [sentence_len, token_len]
         # Get fixed-length sequence and its mask
-        result = list(map(self.trunate_and_pad, tokens_seq_1))
-        seqs = [i[0] for i in result]
-        seq_masks = [i[1] for i in result]
-        seq_segments = [i[2] for i in result]
-        return torch.Tensor(seqs).type(torch.long), torch.Tensor(seq_masks).type(torch.long),torch.Tensor(seq_segments).type(torch.long), torch.Tensor(labels).type(torch.long)
+        result = list(map(self.trunate_and_pad, tokens_seq))
+  
+        input_ids = [i[0] for i in result]
+        attention_mask = [i[1] for i in result]
+        token_type_ids = [i[2] for i in result]
+        
+        return (torch.Tensor(input_ids).type(torch.long), 
+            torch.Tensor(attention_mask).type(torch.long),
+            torch.Tensor(token_type_ids).type(torch.long), 
+            torch.Tensor(labels).type(torch.long)
+        )
     
     
-    def trunate_and_pad(self, tokens_seq_1):
-        # Truncate sequences exceeding the specified length
-        if len(tokens_seq_1) > (self.max_seq_len - 2):
-            tokens_seq_1 = tokens_seq_1[0:(self.max_seq_len - 2)]
-        # Splicing special symbols at the beginning and end respectively
-        seq = ['[CLS]'] + tokens_seq_1 + ['[SEP]']
-        seq_segment = [0] * (len(tokens_seq_1) + 2)
-        # ID
-        seq = self.bert_tokenizer.convert_tokens_to_ids(seq)
-        # Generate padding sequence according to the length of max_seq_len and seq
-        padding = [0] * (self.max_seq_len - len(seq))
-        # create seq_mask
-        seq_mask = [1] * len(seq) + padding
-        # create seq_segment
-        seq_segment = seq_segment + padding
-        # Stitching filler sequences to seq
-        seq += padding
-        assert len(seq) == self.max_seq_len
-        assert len(seq_mask) == self.max_seq_len
-        assert len(seq_segment) == self.max_seq_len
-        return seq, seq_mask, seq_segment
+    def trunate_and_pad(self, tokens_seq):
+        # Concat '[CLS]' at the beginning
+        tokens_seq = ['[CLS]'] + tokens_seq
+        # Truncate sequences of which the lengths exceed the max_seq_len
+        if len(tokens_seq) > max_seq_len:
+            tokens_seq = tokens_seq[0 : max_seq_len]
+        # Generate padding
+        padding = [0] * (max_seq_len - len(tokens_seq)) 
+        # Convert tokens_seq to token_ids
+        input_ids = bert_tokenizer.convert_tokens_to_ids(tokens_seq)
+        input_ids += padding   
+        # Create attention_mask
+        attention_mask = [1] * len(tokens_seq) + padding     
+        # Create token_type_ids
+        token_type_ids = [0] * (max_seq_len)
+        
+        assert len(input_ids) == self.max_seq_len
+        assert len(attention_mask) == self.max_seq_len
+        assert len(token_type_ids) == self.max_seq_len
+        return input_ids, attention_mask, token_type_ids
